33d3d35ad6d1ffeca46f0bd8468a7c2c475f782a
NURDIN-Arnaud-2326082b <arnaud.nurdin@etu.univ-amu.fr>
2024-04-16 09:56:39 +0200

    Exercice 4

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,12 +1,16 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 
 public class Palette extends Application {
 
@@ -25,7 +29,46 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.root = new BorderPane();
+        this.label = new Label();
+        HBox hb = new HBox(label);
+        hb.setAlignment(Pos.CENTER);
+        this.panneau = new Pane();
+        this.vert = new Button("Vert");
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            ++this.nbVert;
+            label.setText( "Vert choisi "+ nbVert +" fois");
+            this.panneau.setStyle("-fx-background-color: green;");
+
+        });
+        this.rouge = new Button("Rouge");
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            ++this.nbRouge;
+            label.setText( "Rouge choisi "+ nbRouge +" fois");
+            this.panneau.setStyle("-fx-background-color: red;");
+        });
+        this.bleu = new Button("Bleu");
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            ++this.nbBleu;
+            label.setText( "Bleu choisi "+ nbBleu +" fois");
+            this.panneau.setStyle("-fx-background-color: blue;");
+            this.label.setAlignment(Pos.CENTER);
+        });
+        this.bas = new HBox(vert, rouge, bleu);
+        this.bas.setAlignment(Pos.CENTER);
+        this.bas.setSpacing(25);
+        this.root.setTop(hb);
+        this.root.setCenter(panneau);
+        this.root.setBottom(bas);
+        Scene scene = new Scene(this.root);
+        primaryStage.setScene( scene );
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(200);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
 
