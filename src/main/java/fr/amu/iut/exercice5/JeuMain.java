fdae890cc5fcad61e3b47031ceb50a5408095f2b
NURDIN-Arnaud-2326082b <arnaud.nurdin@etu.univ-amu.fr>
2024-05-23 08:42:29 +0200

    Quelques améliorations sur tous les tp

--- src/main/java/fr/amu/iut/exercice5/JeuMain.java
+++ src/main/java/fr/amu/iut/exercice5/JeuMain.java
@@ -1,34 +1,31 @@
 package fr.amu.iut.exercice5;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
-import javafx.application.Platform;
-import java.util.ArrayList;
 
 public class JeuMain extends Application {
 
     private Scene scene;
     private BorderPane root;
 
-    //attributs pour gérer le score
+    // Attributs pour gérer le score
     private int nbj1 = 0;
-    //nbj2 est initialisé a -1 car le timer ajoute directement 1 au score du fantome lors de l'éxécution
     private int nbj2 = -1;
     private final Label nb = new Label("   Nombre de victoires : " + nbj1);
     private final Label nb2 = new Label("Nombre de victoires : " + nbj2 + "   ");
@@ -36,166 +33,160 @@ public class JeuMain extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
         root = new BorderPane();
 
-        //Acteurs du jeu
+        // Initialisation de l'interface de démarrage
+        HBox startBox = new HBox();
+        startBox.setAlignment(Pos.CENTER);
+        startBox.setStyle("-fx-background-color: black;");
+        Button startButton = new Button("Start Game");
+        startButton.setStyle("-fx-font-size: 20px; -fx-padding: 10px;");
+        startBox.getChildren().add(startButton);
+
+        root.setCenter(startBox);
+
+        // Gestion du clic sur le bouton de démarrage
+        startButton.setOnAction(event -> {
+            root.setCenter(createGamePane());
+            creerEtDemarrerTimer();
+        });
+
+        scene = new Scene(root);
+
+        primaryStage.setTitle("Pacman1v1.io");
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(1920);  // Largeur totale des trois panneaux (3 x 640)
+        primaryStage.setHeight(1080); // Hauteur totale de la scène
+        primaryStage.show();
+    }
+
+    private Pane createGamePane() {
+        // Acteurs du jeu
         Personnage pacman = new Pacman();
         Personnage fantome = new Fantome();
-        // on positionne le fantôme 20 positions vers la droite
         fantome.setLayoutX(560);
         fantome.setLayoutY(60);
         pacman.setLayoutX(60);
         pacman.setLayoutY(400);
-        //Création des obstacles du jeu
-        Obstacle gauche = new Obstacle(0,0,60,482);
-        Obstacle droite = new Obstacle(580,0,60,480);
-        Obstacle haut = new Obstacle(0,0,640,60);
-        Obstacle bas = new Obstacle(0,420,640,62);
-        Obstacle mid = new Obstacle(400,260,20,80);
-        Obstacle mid2 = new Obstacle(400,260,80,20);
-        Obstacle mid3 = new Obstacle(160,200,80,20);
-        Obstacle mid4 = new Obstacle(220,140,20,80);
-        ArrayList<Obstacle> obstacles = new ArrayList<>();
-        obstacles.add(gauche);
-        obstacles.add(droite);
-        obstacles.add(haut);
-        obstacles.add(bas);
-        obstacles.add(mid);
-        obstacles.add(mid2);
-        obstacles.add(mid3);
-        obstacles.add(mid4);
 
-        //panneau du jeu
+        // Création des obstacles du jeu
+        ArrayList<Obstacle> obstacles = new ArrayList<>();
+        obstacles.add(new Obstacle(0, 0, 60, 482));
+        obstacles.add(new Obstacle(580, 0, 60, 480));
+        obstacles.add(new Obstacle(0, 0, 640, 60));
+        obstacles.add(new Obstacle(0, 420, 640, 62));
+        obstacles.add(new Obstacle(400, 260, 20, 80));
+        obstacles.add(new Obstacle(400, 260, 80, 20));
+        obstacles.add(new Obstacle(160, 200, 80, 20));
+        obstacles.add(new Obstacle(220, 140, 20, 80));
+
+        // Panneau du jeu
         Pane jeu = new Pane();
         jeu.setPrefSize(640, 480);
-        jeu.getChildren().add(pacman);
-        jeu.getChildren().add(fantome);
-        for (Obstacle obstacle : obstacles) {
-            jeu.getChildren().add(obstacle);
-        }
+        jeu.getChildren().addAll(pacman, fantome);
+        jeu.getChildren().addAll(obstacles);
         jeu.setStyle("-fx-background-color: red;");
 
-        //création des interfaces publicitaires à gauche et à droite de l'écran du jeu
+        // Création des interfaces publicitaires à gauche et à droite de l'écran du jeu
         VBox pubGauche = new VBox();
-        pubGauche.setPrefHeight(1080);
-        pubGauche.setPrefWidth(640);
+        pubGauche.setPrefSize(640, 1080);
         pubGauche.setStyle("-fx-background-color: BLACK;");
-        root.setLeft(pubGauche);
         VBox pubDroite = new VBox();
-        pubDroite.setPrefHeight(1080);
-        pubDroite.setPrefWidth(640);
+        pubDroite.setPrefSize(640, 1080);
         pubDroite.setStyle("-fx-background-color: BLACK;");
-        root.setRight(pubDroite);
 
-        //Création de l'écran d'affichage du jeu (vbox du milieu)
+        // Création de l'écran d'affichage du jeu (VBox du milieu)
         VBox ecran = new VBox();
-        ecran.setPrefHeight(1080);
-        ecran.setPrefWidth(640);
+        ecran.setPrefSize(640, 1080);
 
-        //Création de l'interface sous l'écran de jeu
+        // Création de l'interface sous l'écran de jeu
         HBox miseEnPage = new HBox();
         Separator sep5 = new Separator(Orientation.VERTICAL);
         sep5.setStyle("-fx-background-color: white;");
         Separator sep6 = new Separator(Orientation.VERTICAL);
         sep6.setStyle("-fx-background-color: white;");
-        //Création de l'écran de score
+
+        // Création de l'écran de score
         HBox score = new HBox();
         VBox scorej1 = new VBox();
         Label nom = new Label("   Pacman");
         nom.setTextFill(Color.WHITE);
-        nom.setFont( Font.font("Courier", FontWeight.NORMAL, 25) );
-        HBox nomm = new HBox();
-        nomm.getChildren().add(nom);
+        nom.setFont(Font.font("Courier", FontWeight.NORMAL, 25));
+        HBox nomm = new HBox(nom);
         nomm.setAlignment(Pos.CENTER_LEFT);
 
         nb.setTextFill(Color.WHITE);
-        nb.setFont( Font.font("Courier", FontWeight.NORMAL, 20) );
-        HBox nbb = new HBox();
-        nbb.getChildren().add(nb);
+        nb.setFont(Font.font("Courier", FontWeight.NORMAL, 20));
+        HBox nbb = new HBox(nb);
         nbb.setAlignment(Pos.BOTTOM_LEFT);
         Label espace = new Label("      ");
         scorej1.getChildren().addAll(nomm, espace, nbb);
-        nom.setLayoutY(250);
-        scorej1.setPrefHeight(500);
-        scorej1.setPrefWidth(320);
+        scorej1.setPrefSize(320, 500);
         scorej1.setAlignment(Pos.CENTER);
+
         VBox scorej2 = new VBox();
         Label nom2 = new Label("Fantome  ");
         nom2.setTextFill(Color.WHITE);
-        nom2.setFont( Font.font("Courier", FontWeight.NORMAL, 25) );
-        HBox nomm2 = new HBox();
-        nomm2.getChildren().add(nom2);
+        nom2.setFont(Font.font("Courier", FontWeight.NORMAL, 25));
+        HBox nomm2 = new HBox(nom2);
         nomm2.setAlignment(Pos.CENTER_RIGHT);
+
         nb2.setTextFill(Color.WHITE);
-        nb2.setFont( Font.font("Courier", FontWeight.NORMAL, 20) );
-        HBox nbb2 = new HBox();
-        nbb2.getChildren().add(nb2);
+        nb2.setFont(Font.font("Courier", FontWeight.NORMAL, 20));
+        HBox nbb2 = new HBox(nb2);
         nbb2.setAlignment(Pos.BOTTOM_RIGHT);
         Label espace2 = new Label("      ");
-        scorej2.getChildren().addAll(nomm2,espace2, nbb2);
-        scorej2.setPrefHeight(500);
-        scorej2.setPrefWidth(320);
+        scorej2.getChildren().addAll(nomm2, espace2, nbb2);
+        scorej2.setPrefSize(320, 500);
         scorej2.setAlignment(Pos.CENTER);
-        score.getChildren().addAll(scorej1,scorej2);
-        score.setPrefHeight(500);
-        score.setPrefWidth(640);
-        miseEnPage.setPrefHeight(500);
-        miseEnPage.setPrefWidth(640);
+
+        score.getChildren().addAll(scorej1, scorej2);
+        score.setPrefSize(640, 500);
         score.setStyle("-fx-background-color: BLACK;");
 
-        //Création du titre au milieu du jeu
+        // Création du titre au milieu du jeu
         Label titre = new Label("PACMAN 1V1");
         titre.setStyle("-fx-background-color: black;");
         titre.setTextFill(Color.WHITE);
-        titre.setFont( Font.font("Courier", FontWeight.NORMAL, 30) );
-        miseEnPage.getChildren().addAll(sep5,score,sep6);
+        titre.setFont(Font.font("Courier", FontWeight.NORMAL, 30        ));
+        miseEnPage.getChildren().addAll(sep5, score, sep6);
         Separator sep = new Separator(Orientation.HORIZONTAL);
         sep.setStyle("-fx-background-color: white;");
         Separator sep4 = new Separator(Orientation.HORIZONTAL);
         sep4.setStyle("-fx-background-color: white;");
-        ecran.getChildren().addAll(jeu,titre,sep,miseEnPage,sep4);
+
+        ecran.getChildren().addAll(jeu, titre, sep, miseEnPage, sep4);
         ecran.setAlignment(Pos.CENTER);
         HBox hbox = new HBox();
         Separator sep2 = new Separator(Orientation.VERTICAL);
         sep2.setStyle("-fx-background-color: white;");
         Separator sep3 = new Separator(Orientation.VERTICAL);
         sep3.setStyle("-fx-background-color: white;");
-        hbox.getChildren().addAll(pubGauche,ecran,pubDroite);
+        hbox.getChildren().addAll(pubGauche, ecran, pubDroite);
         hbox.setStyle("-fx-background-color: black;");
-        root.setCenter(hbox);
-        //on construit une scene 640 * 480 pixels
-        scene = new Scene(root);
+        return hbox;
+    }
 
-        //Gestion du déplacement du personnage
-        deplacer(pacman, fantome,obstacles);
-
-        // Création du timer
+    private void creerEtDemarrerTimer() {
         timer = new Timer();
-
-        // Planification de la tâche à exécuter toutes les 60 secondes
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                // Incrémenter le score du fantôme
                 nbj2++;
                 Platform.runLater(() -> nb2.setText("Nombre de victoires : " + nbj2));
             }
         }, 0, 30000);
-
-        primaryStage.setTitle("Pacman1v1.io");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
+
     /**
      * Permet de gérer les événements de type clavier, pression des touches
      * pour le j1(up,down, right, left), pour le j2( z,q,s,d)
      *
-     * @param j1 joueur 1
-     * @param j2 joueur 2
-     * @param obstacle liste d'obstacles présent sur la map
+     * @param j1      joueur 1
+     * @param j2      joueur 2
+     * @param obstacles liste d'obstacles présent sur la map
      */
-    private void deplacer(Personnage j1, Personnage j2, ArrayList<Obstacle> obstacle) {
+    private void deplacer(Personnage j1, Personnage j2, ArrayList<Obstacle> obstacles) {
         scene.setOnKeyPressed((KeyEvent event) -> {
             switch (event.getCode()) {
                 case LEFT:
@@ -222,41 +213,35 @@ public class JeuMain extends Application {
                 case S:
                     j2.deplacerEnBas(scene.getHeight());
                     break;
-
             }
-            if (j1.estEnCollision(j2)){
-                ++nbj1;
+            if (j1.estEnCollision(j2)) {
+                nbj1++;
                 nb.setText("   Nombre de victoires : " + nbj1);
                 j2.setLayoutX(560);
                 j2.setLayoutY(60);
                 j1.setLayoutX(60);
                 j1.setLayoutY(400);
+
                 // Arrêt du timer existant et création d'un nouveau
                 timer.cancel();
                 nbj2--;
-                timer = new Timer();
-                timer.scheduleAtFixedRate(new TimerTask() {
-                    @Override
-                    public void run() {
-                        nbj2++;
-                        Platform.runLater(() -> nb2.setText("Nombre de victoires : " + nbj2));
-                    }
-                }, 0, 30000);
+                creerEtDemarrerTimer();
             }
-            for (Obstacle value : obstacle) {
+            for (Obstacle value : obstacles) {
                 if (value.collisionMur(j1)) {
                     j1.setLayoutX(j1.getPosX());
                     j1.setLayoutY(j1.getPosY());
-
                 }
                 if (value.collisionMur(j2)) {
                     j2.setLayoutX(j2.getPosX());
                     j2.setLayoutY(j2.getPosY());
-
                 }
             }
         });
     }
 
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
--- src/main/java/fr/amu/iut/exercice9/Animation.java
+++ src/main/java/fr/amu/iut/exercice9/Animation.java
@@ -1,9 +1,10 @@
 package fr.amu.iut.exercice9;
 
+import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -11,21 +12,62 @@ public class Animation extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane root = new BorderPane();
+        StackPane root = new StackPane();
         CustomButton customButton = new CustomButton();
-        root.setCenter(customButton);
+        root.getChildren().add(customButton);
         Scene scene = new Scene(root, 400, 400);
 
-        Duration duration = Duration.millis(1500);
-        TranslateTransition transition1 = new TranslateTransition(duration, customButton);
-        transition1.setByX(150);
-        transition1.setByY(-150);
-        transition1.setAutoReverse(true);
-        transition1.setCycleCount(2);
+        Duration duration = Duration.millis(500);
 
-//        SequentialTransition st = new SequentialTransition(transition1, transition2, transition3, transition4, transition5);
+        TranslateTransition transitionRight = new TranslateTransition(duration, customButton);
+        transitionRight.setByX(150);
 
-        customButton.setOnMousePressed(mouseEvent -> transition1.play());
+        TranslateTransition transitionDown = new TranslateTransition(duration, customButton);
+        transitionDown.setByY(150);
+
+        TranslateTransition transitionLeft = new TranslateTransition(duration, customButton);
+        transitionLeft.setByX(-300);
+
+        TranslateTransition transitionUp = new TranslateTransition(duration, customButton);
+        transitionUp.setByY(-300);
+
+        TranslateTransition transitionRightBack = new TranslateTransition(duration, customButton);
+        transitionRightBack.setByX(300);
+
+        TranslateTransition transitionDownBack = new TranslateTransition(duration, customButton);
+        transitionDownBack.setByY(150);
+
+        TranslateTransition transitionCenter = new TranslateTransition(duration, customButton);
+        transitionCenter.setByX(-150);
+
+        //retour en arrière
+
+        TranslateTransition transitionUp2 = new TranslateTransition(duration, customButton);
+        transitionUp2.setByY(-150);
+
+        TranslateTransition transitionRight2 = new TranslateTransition(duration, customButton);
+        transitionRight2.setByX(150);
+
+        TranslateTransition transitionDown2 = new TranslateTransition(duration, customButton);
+        transitionDown2.setByY(300);
+
+        TranslateTransition transitionLeft2 = new TranslateTransition(duration, customButton);
+        transitionLeft2.setByX(-300);
+
+        TranslateTransition transitionRightBack2 = new TranslateTransition(duration, customButton);
+        transitionRightBack2.setByX(300);
+
+        TranslateTransition transitionUpToCenter2 = new TranslateTransition(duration, customButton);
+        transitionUpToCenter2.setByY(-150);
+
+        TranslateTransition transitionRightToCenter2 = new TranslateTransition(duration, customButton);
+        transitionRightToCenter2.setByX(-150);
+
+        SequentialTransition sequentialTransition = new SequentialTransition(
+                transitionRight, transitionDown, transitionLeft, transitionUp, transitionRightBack, transitionDownBack, transitionCenter,transitionRight2,transitionUp2,transitionLeft2,  transitionDown2,transitionRightBack2,  transitionUpToCenter2, transitionRightToCenter2);
+        sequentialTransition.setCycleCount(1);
+
+        customButton.setOnMousePressed(mouseEvent -> sequentialTransition.play());
 
         primaryStage.setTitle("Animation");
         primaryStage.setScene(scene);
